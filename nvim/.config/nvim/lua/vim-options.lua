f8ed1d9b0385c9a6acb944a0c9ff6f4535588d56
jaccoobi <jacobgundlach13@outlook.com>
2024-09-30 21:58:15 -0400

    Checked nvim commands

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -53,6 +53,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
